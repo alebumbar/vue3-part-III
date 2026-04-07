af28cddad232f6286c30cefec62dfe31832be2ad
Alexandra Bumbar(work) <alexandrabumbar@MacBook-Pro.local>
2026-04-07 19:49:31 +0300

    imporving methods

--- scripts.js
+++ scripts.js
@@ -1,27 +1,36 @@
 //testing
-var todos = [
-{
-  text: 'Learn Vuejs',
-  done: true
-}
-{
-  text: 'Learn Vuejs',
-  done: false
-}
+var items = [
+  {
+    text: "Learn Vuejs",
+    done: true,
+  },
+
+  {
+    text: "Learn Vuejs",
+    done: false,
+  },
 ];
 
-const todosApp= {
+const todosApp = {
   data() {
     return {
-      todos: window.todos,
-      items: [],
-      newToDo: { item: '' },
+      items: window.items,
+      //items: [],
+      newToDo: {
+        done: false,
+      },
     };
   },
   methods: {
     addItem: function () {
-      this.items.push(this.newToDo);
-      this.newToDo = {};
+      if (this.newToDo.text) {
+        this.items.push(this.newToDo);
+        this.newToDo = {
+          done: false,
+        };
+      } else {
+        alert("To do item is required");
+      }
     },
     clearAll: function () {
       this.items = [];
